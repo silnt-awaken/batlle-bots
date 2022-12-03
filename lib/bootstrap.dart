60e793ca9919d23058e286ef38e5a878307b014f
mike-gallego-2 <mgallego.business@gmail.com>
2022-12-03 11:13:19 -0500

    :sparkles: add landscape mode for android or ios

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError =
       (details) => log(details.exceptionAsString(), stackTrace: details.stack);
+  WidgetsFlutterBinding.ensureInitialized();
+  // set the landscape orientation for android devices
+  if (Platform.isAndroid || Platform.isIOS) {
+    await SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  }
+
   runApp(await builder());
 }
